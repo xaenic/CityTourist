0aab9eff49cac778fbfff2bbbd679890225dd60f
Kiwi <allanvillegas35@gmail.com>
2024-05-23 13:00:21 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CityTourist.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
