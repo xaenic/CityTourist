5801f9fda317eaa9703d4d3574148c00429d5177
JmReaper <johnmburila@gmail.com>
2024-05-23 16:01:17 +0800

    seed.data

---
+++ Models/SeedData.cs
@@ -0,0 +1,30 @@
+﻿﻿using Microsoft.EntityFrameworkCore;
+
+namespace CityTourist.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            TouristDbContext context = app.ApplicationServices
+                .CreateScope().ServiceProvider
+                .GetRequiredService<TouristDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            if (!context.Admin.Any())
+            {
+                context.Admin.AddRange(
+                new AdminModel
+                {
+                    UserName = "Admin",
+                    Password = "Admin",
+                    Name = "Kira",
+            
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
