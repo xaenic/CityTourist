016c1d2a845f5b16864fa57f2da9012d44e11d7e
Kiwi <allanvillegas35@gmail.com>
2024-05-25 16:48:18 +0800

    authorization added

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,8 +1,10 @@
 ﻿using CityTourist.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityTourist.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         public IActionResult Index()
